b8f38537ebda957864a7151fbee6b95f6df2c8a4
Ali Ahmadi <aliahmadi79sh@gmail.com>
2024-09-06 09:35:36 +0330

    Final Commit For Push

---
+++ utilities/utils.py
@@ -0,0 +1,19 @@
+class dict2w(dict):
+    ''' creates a two-way dictionary
+        by adding one key-value pair, a value-key pair is added as well
+    '''
+    def __setitem__(self, key, value):
+        if key in self:
+            del self[key]
+        if value in self:
+            del self[value]
+        dict.__setitem__(self, key, value)
+        dict.__setitem__(self, value, key)
+
+    def __delitem__(self, key):
+        dict.__delitem__(self, self[key])
+        dict.__delitem__(self, key)
+
+    def __len__(self):
+        return dict.__len__(self) // 2
+    
